4ecf38612e3fb7968998be3cf94751e3c99dcc40
Santiago Ordonez <santiagoordonez23@gmail.com>
2016-02-11 22:37:37 -0500

    Create location.py

---
+++ location.py
@@ -0,0 +1,52 @@
+class Location:
+    def __init__(self, row, column):
+        """Initialize a location.
+
+        @type self: Location
+        @type row: int
+        @type column: int
+        @rtype: None
+        """
+        # TODO
+
+        self.row = row #Unsure <<
+        self.column = column
+
+    def __str__(self):
+        """Return a string representation.
+
+        @rtype: str
+        """
+        # TODO
+        pass
+
+    def __eq__(self, other):
+        """Return True if self equals other, and false otherwise.
+
+        @rtype: bool
+        """
+        # TODO
+        return (self.row == other.row and self.column == other.column)
+
+
+def manhattan_distance(origin, destination):
+    """Return the Manhattan distance between the origin and the destination.
+
+    @type origin: Location
+    @type destination: Location
+    @rtype: int
+    """
+    # TODO
+    pass
+
+
+def deserialize_location(location_str):
+    """Deserialize a location.
+
+    @type location_str: str
+        A location in the format 'row,col'
+    @rtype: Location
+    """
+    # TODO
+    location  = Location(int(location_str[0]),int(location_str[1]))
+    return location
